5ce6462914634dc6f2d9868bdb63bd3aeb156fc2
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-12-03 11:21:58 +0200

    add user route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from uuid import uuid4
+from uuid import UUID, uuid4
 from fastapi import FastAPI
 from typing import List
 from models import Gender, Roles, User
@@ -7,14 +7,14 @@ app = FastAPI()
 
 db: List[User] = [
     User(
-        id=uuid4(), 
+        id=UUID("bd7ef9e7-b56a-4cfa-9717-3a765c9c4189"), 
         first_name="Jamilla", 
         last_name="Ahmed",
         gender=Gender.female,
         role=[Roles.student]
         ),
     User(
-        id=uuid4(), 
+        id=UUID("946d4dba-cb40-4453-b94d-572ffcebf800"), 
         first_name="Kudya", 
         last_name="Makumboarimumvura",
         middle_name="Usavi",
@@ -26,8 +26,19 @@ db: List[User] = [
 
 @app.get("/")
 def root():
+    """Base route.
+    Returns: a simple JSON response
+    """
     return {"Hello": "Mason"}
 
 @app.get("/api/v1/users")
 def fetch_users():
+    """Get users route.
+    Returns: Full list of users in database.
+    """
     return db
+
+@app.post("/api/v1/users")
+def create_user(user: User):
+    db.append(user)
+    return {"id": user.id}
